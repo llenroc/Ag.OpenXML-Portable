984c6ec8c8ed86aa8e125894b79d0eb9f1a48eb6
SND\ZeroKoll_cp <SND\ZeroKoll_cp@9b283d60-5439-405e-af05-b73fd8c4d996>
2010-02-11 05:18:48 +0000

    A missed file... Oops...
    git-svn-id: https://agopenxml.svn.codeplex.com/svn@40556 9b283d60-5439-405e-af05-b73fd8c4d996

---
+++ FiftyNine.Ag.OpenXML.Word/Parts/DefaultDocumentPart.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Net;
+using System.Windows;
+using FiftyNine.Ag.OpenXML.Common.Packaging;
+using FiftyNine.Ag.OpenXML.Word.Constants;
+using FiftyNine.Ag.OpenXML.Common.Extensions;
+using FiftyNine.Ag.OpenXML.Word.Utilities;
+using System.Linq;
+using FiftyNine.Ag.OpenXML.Word.Elements;
+
+namespace FiftyNine.Ag.OpenXML.Word.Parts
+{
+    public class DefaultDocumentPart : DocumentPart
+    {
+        protected override string ContentType
+        {
+            get
+            {
+                return ContentTypes.MainDocument;
+            }
+        }
+    }
+}
